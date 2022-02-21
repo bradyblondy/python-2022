5d0940cc37183fae77ca54d5748333045ae54eec
bradyblondy <100097079+bradyblondy@users.noreply.github.com>
2022-02-22 12:14:24 +1300

    Did first exercixe

---
+++ 1_print.py
@@ -0,0 +1,12 @@
+#print your own name
+print("liam brady")
+#print your teachers name
+print("luke smith")
+#print your age
+print("15")\
+#print out how many hours you have in your favourite game
+print("250 hours")
+#print out what year it will be in 25 years
+print(2022+25)
+#print out how old you will be in 100 years 
+print(16+100)
