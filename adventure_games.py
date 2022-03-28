2fe1e18a7187eede2cc4a33aa11c9d5d16c4fb62
bradyblondy <100097079+bradyblondy@users.noreply.github.com>
2022-03-29 11:45:55 +1300

    todays work

--- adventure_games.py
+++ adventure_games.py
@@ -7,7 +7,14 @@
 #Define Rooms
 ########################
 ########################
-office = 
+Gym = print("a big room full of gym equipment")
+office = print("A room full of desks and computers")
+library = Print("A big room full of shelves full of books")
+sciencelab1 = ("A science room full of science equipment and lots of flammable liquids")
+sciencelab2 = ("A science room full of science equipment and lots of flammable liquids")
+sciencelab3 = ("A science room full of science equipment and lots of flammable liquids")
+sciencelab4 = ("A science room full of science equipment and lots of flammable liquids")
+
 ########################
 #Define Connections
 ########################
